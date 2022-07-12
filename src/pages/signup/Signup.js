22b3ea3b8af9f328bd7f396b118b9e05ad3f574c
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-12 15:17:49 -0500

    Clase 157

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,7 +1,50 @@
+import { useState } from 'react'
 import './Signup.css'
 
 export const Signup = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [displayName, setDisplayName] = useState('')
+  const [thumbnail, setThumbnail] = useState(null)
+
   return (
-    <div>Signup</div>
+    <form className='auth-form'>
+      <h2>Sign up</h2>
+      <label>
+        <span>email:</span>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          type="email"
+          required
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          type="password"
+          required
+        />
+      </label>
+      <label>
+        <span>display name:</span>
+        <input
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+          type="text"
+          required
+        />
+      </label>
+      <label>
+        <span>profile thumbnail:</span>
+        <input
+          type="file"
+          required
+        />
+      </label>
+      <button className="btn">Sign up</button>
+    </form>
   )
 }
