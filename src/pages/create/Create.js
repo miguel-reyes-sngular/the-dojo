eefd56a223c19bc2212eb2372a0bb32c5232192c
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-13 16:35:33 -0500

    Clase 174

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -20,6 +20,7 @@ export const Create = () => {
   const [dueDate, setDueDate] = useState('')
   const [category, setCategory] = useState('')
   const [assignedUsers, setAssignedUsers] = useState([])
+  const [formError, setFormError] = useState(null)
 
   useEffect(() => {
     if (documents) {
@@ -32,6 +33,17 @@ export const Create = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setFormError(null)
+
+    if (!category) {
+      setFormError('Please select a project category')
+      return
+    }
+    if(assignedUsers.length < 1) {
+      setFormError('Please assign the project to at least 1 user')
+      return
+    }
+
     console.log(name, details, dueDate, category.value, assignedUsers)
   }
 
@@ -87,6 +99,8 @@ export const Create = () => {
         </label>
 
         <button className='btn'>Add project</button>
+
+        {formError && <p className='error'>{formError}</p>}
       </form>
     </div>
   )
