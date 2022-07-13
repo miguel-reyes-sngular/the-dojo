863569731ed2f079a21a13b3894e885c2423683d
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-13 17:55:05 -0500

    Clase 177

---
+++ src/components/ProjectList.js
@@ -0,0 +1,14 @@
+import './ProjectList.css'
+
+export const ProjectList = ({ projects }) => {
+  return (
+    <div>
+      {projects.length === 0 && <p>No projects yet!</p>}
+      {projects.map((project)=> (
+        <div key={project.id}>
+          {project.name}
+        </div>
+      ))  }
+    </div>
+  )
+}
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,7 +1,15 @@
+import { useCollection } from '../../hooks/useCollection'
+import { ProjectList } from '../../components/ProjectList'
 import './Dashboard.css'
 
 export const Dashboard = () => {
+  const { documents, error } = useCollection('projects')
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <h2 className='page-title'>Dashboard</h2>
+      {error && <p className='error'>{error}</p>}
+      {documents && <ProjectList projects={documents} />}
+    </div>
   )
 }
