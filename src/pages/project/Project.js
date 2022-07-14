75f8d051129dc55df20fad2f85fe2df2aef25360
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-14 13:04:39 -0500

    Clase 181

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom'
 import { useDocument } from '../../hooks/useDocument'
+import { ProjectSummary } from './ProjectSummary'
 
 import './Project.css'
 
@@ -17,7 +18,7 @@ export const Project = () => {
 
   return (
     <div className='project-details'>
-      <h1>{document.name}</h1>
+      <ProjectSummary project={document} />
     </div>
   )
 }
