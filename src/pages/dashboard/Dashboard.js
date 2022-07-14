cb652ae91cedcaadc492145cd443932fe78216b8
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-14 17:00:21 -0500

    Clase 187

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,5 +1,7 @@
 import { useCollection } from '../../hooks/useCollection'
 import { ProjectList } from '../../components/ProjectList'
+import { ProjectFilter } from './ProjectFilter'
+
 import './Dashboard.css'
 
 export const Dashboard = () => {
@@ -9,6 +11,7 @@ export const Dashboard = () => {
     <div>
       <h2 className='page-title'>Dashboard</h2>
       {error && <p className='error'>{error}</p>}
+      {documents && <ProjectFilter />}
       {documents && <ProjectList projects={documents} />}
     </div>
   )
