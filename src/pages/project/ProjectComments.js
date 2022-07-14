ca484f45dbaa6dff072ae7e381b3ecec8ace313e
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-14 18:15:37 -0500

    Clase 191

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -3,6 +3,7 @@ import { timestamp } from "../../firebase/config"
 import { useAuthContext } from '../../hooks/useAuthContext'
 import { useFirestore } from "../../hooks/useFirestore"
 import { Avatar } from "../../components/Avatar"
+import formatDistanceToNow from "date-fns/formatDistanceToNow"
 
 export const ProjectComments = ({ project }) => {
   const { updateDocument, response } = useFirestore('projects')
@@ -42,7 +43,7 @@ export const ProjectComments = ({ project }) => {
               <p>{comment.displayName}</p>
             </div>
             <div className="comment-date">
-              <p>Date here</p>
+              <p>{formatDistanceToNow(comment.createdAt.toDate(), { addSuffix: true })}</p>
             </div>
             <div className="comment-content">
               <p>{comment.content}</p>
