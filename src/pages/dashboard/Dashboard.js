9594239a9dffcf0e92acad6aaa218c41cb311dfd
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-14 17:36:34 -0500

    Clase 189

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -2,16 +2,39 @@ import { useState } from 'react'
 import { useCollection } from '../../hooks/useCollection'
 import { ProjectList } from '../../components/ProjectList'
 import { ProjectFilter } from './ProjectFilter'
+import { useAuthContext } from '../../hooks/useAuthContext'
 
 import './Dashboard.css'
 
 export const Dashboard = () => {
+  const { user } = useAuthContext()
   const { documents, error } = useCollection('projects')
   const [currentFilter, setCurrentFilter] = useState('all')
 
   const changeFilter = (newFilter) => {
     setCurrentFilter(newFilter)
   }
+
+  const projects = documents ? documents.filter((document) => {
+    switch (currentFilter) {
+      case 'all':
+        return true
+      case 'mine':
+        let assignedToMe = false
+        document.assignedUsersList.forEach((u) => {
+          if (user.uid === u.id) {
+            assignedToMe = true
+          }
+        })
+        return assignedToMe
+      case 'development':
+      case 'design':
+      case 'sales':
+      case 'marketing':
+        return document.category === currentFilter
+      default: return true
+    }
+  }) : null;
 
   return (
     <div>
@@ -21,7 +44,7 @@ export const Dashboard = () => {
         currentFilter={currentFilter}
         changeFilter={changeFilter} />
       }
-      {documents && <ProjectList projects={documents} />}
+      {projects && <ProjectList projects={projects} />}
     </div>
   )
 }
